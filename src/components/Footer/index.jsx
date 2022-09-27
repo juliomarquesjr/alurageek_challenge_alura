6958dc8e02816f75e0c7b277690d9ce943c0beec
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-09-27 15:14:43 -0300

    Add componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./estilos.css"
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="Logo AluraGeek" />
+      </div>
+      <div>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div>
+      <div>
+        <p>Fale Conosco</p>
+      </div>
+    </div>
+  );
+};
